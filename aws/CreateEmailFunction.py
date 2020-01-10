5a7733f807029124edc1cc28b45daf8927723c7f
Vittorio Nardone <vittorio.nardone@gmail.com>
2020-01-10 19:25:45 +0100

    API session ID wip

---
+++ aws/CreateEmailFunction.py
@@ -0,0 +1,124 @@
+import boto3
+from botocore.exceptions import ClientError
+import http.client, urllib.parse
+import json
+import os
+import logging
+import time
+import re
+import uuid
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource("dynamodb")
+addresses_table = dynamodb.Table(os.environ['addresses_table_name'])
+sessions_table = dynamodb.Table(os.environ['sessions_table_name'])
+
+ssm = boto3.client('ssm')
+valid_domains = os.environ['valid_domains'].split(',')
+defaultTTL = int(os.environ['mailbox_ttl'])
+recaptcha_key = os.environ['recaptcha_key']
+
+def address_exists(address):
+    exists = False
+    try:
+        response = addresses_table.get_item(
+            Key={
+            'address': address
+            }
+        )
+    except ClientError as e:
+        logger.info('## DynamoDB Client Exception')
+        logger.info(e.response['Error']['Message'])
+    else:
+        if 'Item' in response:
+            item = response['Item']
+            if item['TTL'] > int(time.time()):
+                exists = True
+    return exists
+
+def create_address(address):
+    TTL = int(time.time()) + defaultTTL
+    try:
+        response = addresses_table.put_item(
+            Item= {'address': address, 'TTL': TTL}
+        )
+    except ClientError as e:
+        logger.info('## DynamoDB Client Exception')
+        logger.info(e.response['Error']['Message'])
+
+def create_session():
+    session_id = str(uuid.uuid4())
+    TTL = int(time.time()) + defaultTTL
+    try:
+        response = sessions_table.put_item(
+            Item= {'sessionID': session_id, 'TTL': TTL}
+        )
+    except ClientError as e:
+        logger.info('## DynamoDB Client Exception')
+        logger.info(e.response['Error']['Message'])
+    return session_id
+                    
+def validate_email(address):
+    valid = False
+    # Do some basic regex validation 
+    match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', address)
+    if (match != None):
+        domain = address.split("@")[-1]
+        if domain in valid_domains:
+            valid = True
+    return valid
+
+def validate_recaptcha(token):
+    conn = http.client.HTTPSConnection('www.google.com')
+    headers = {"Content-type": "application/x-www-form-urlencoded"}
+    params = urllib.parse.urlencode({'secret': recaptcha_key, 'response': token}) 
+    conn.request('POST', '/recaptcha/api/siteverify', params, headers)
+    response = conn.getresponse()
+    r = response.read().decode() 
+    logger.info('## reCAPTCHA response')
+    logger.info(r)
+    r = json.loads(r)
+    return r['success']
+                        
+def lambda_handler(event, context):
+    logger.info('## ENVIRONMENT VARIABLES')
+    logger.info(os.environ)
+    logger.info('## EVENT')
+    logger.info(event)
+    logger.info('## Configuration')
+    logger.info('Domains: {}'.format(valid_domains))
+    logger.info('TTL: {}'.format(defaultTTL))
+    
+    headers = {
+                "access-control-allow-headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "access-control-allow-methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+                "access-control-allow-origin": "*"
+            }
+    
+    result = {"statusCode": 400, 
+            "body": json.dumps({"message":"missing or invalid parameters"}), 
+            "headers": headers
+    }
+
+    if event["queryStringParameters"] != None:
+        if (("address" in event["queryStringParameters"]) and 
+            ("captcha" in event["queryStringParameters"])):
+            email_address = event["queryStringParameters"]["address"] 
+            captcha = event["queryStringParameters"]["captcha"] 
+            if validate_recaptcha(captcha):
+                if validate_email(email_address):
+                    if address_exists(email_address):
+                        message = "email address already exists"
+                    else:
+                        create_address(email_address)
+                        message = "email address created"
+                    
+                    result = {
+                            "statusCode": 200,
+                            "body": json.dumps({"message":message, "sessionid": create_session()}), 
+                            "headers": headers
+                    }
+    
+    return result     
---
+++ aws/GetEmailsListFunction.py
@@ -0,0 +1,77 @@
+import boto3
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key
+import json
+import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource("dynamodb")
+emails_table = dynamodb.Table(os.environ['emails_table_name'])
+sessions_table = dynamodb.Table(os.environ['sessions_table_name'])
+
+def get_emails(destination):
+    items = None
+    try:
+        filtering_exp = Key('destination').eq(destination)
+        response = emails_table.query(KeyConditionExpression=filtering_exp)        
+    except ClientError as e:
+        logger.info('## DynamoDB Client Exception')
+        logger.info(e.response['Error']['Message'])
+    else:
+        #Clean response       
+        for i in response['Items']:
+            i.pop('bucketObjectKey', None)
+            i.pop('bucketName', None)
+        items = {'Items' : response['Items'], 'Count' : response['Count']}
+    return items
+
+def session_is_valid(sessionid):
+    valid = False
+    try:
+        response = sessions_table.get_item(
+            Key={
+            'sessionId': sessionid
+            }
+        )
+    except ClientError as e:
+        logger.info('## DynamoDB Client Exception')
+        logger.info(e.response['Error']['Message'])
+    else:
+        if 'Item' in response:
+            item = response['Item']
+            if item['TTL'] > int(time.time()):
+                valid = True
+    return valid
+ 
+
+def lambda_handler(event, context):
+    logger.info('## ENVIRONMENT VARIABLES')
+    logger.info(os.environ)
+    logger.info('## EVENT')
+    logger.info(event)
+    
+    headers = {
+                "access-control-allow-headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "access-control-allow-methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+                "access-control-allow-origin": "*"
+            }
+    
+    result = {"statusCode": 400, 
+            "body": json.dumps({"message":"missing or invalid parameters"}), 
+            "headers": headers
+    }
+
+    if event["pathParameters"] != None:
+        
+        if "sessionid" in event["queryStringParameters"]:
+            if session_is_valid(event["queryStringParameters"]["sessionid"]):
+                if "destination" in event["pathParameters"]:
+                    destination = event["pathParameters"]["destination"]
+                    items = get_emails(destination)
+                    if items != None:
+                        result = {"statusCode": 200, "body": json.dumps(items), "headers": headers }
+
+    return result    
